99556585d3605f4fa047fc65fc6c5272df8b87a3
zhyoleh <zhinticholeh@gmail.com>
2022-03-24 09:21:55 -0600

    timer working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,18 +7,18 @@ import {
   useTexture,
 } from "@react-three/drei";
 import glsl from "babel-plugin-glsl/macro";
-import { Suspense, useEffect, useRef } from "react";
+import { Suspense, useEffect, useRef, useState } from "react";
 import * as THREE from "three";
 import backgroundImage from "./images/artboard.png";
-
 
 const ExplosionMaterial = shaderMaterial(
   //Uniform
   {
-    uTime: 0,
+    uTime: 0.0,
     uColor: new THREE.Color(0.0, 0.0, 0.0),
     uTexture: new THREE.Texture(),
-    uCustomUv: new THREE.Vector2()
+    uCustomUv: new THREE.Vector2(),
+    uCreationTimer: 0.0,
   },
   // Vertex Shader
   glsl`
@@ -39,20 +39,20 @@ const ExplosionMaterial = shaderMaterial(
     uniform float uTime;
     uniform sampler2D uTexture;
     uniform vec2 uCustomUv;
+    uniform float uCreationTimer;
 
     varying vec2 vUv;
 
     void main(){
 
-      float strength = distance(vUv, uCustomUv);
+      float strength = distance(vUv, uCustomUv) / uCreationTimer;
 
       if( strength > 0.1){
         strength = 1.0;
       } else if( strength < 0.1){
-        strength = 0.0;
+        strength = 0.0; //this is the circle
       }
-
-      vec3 texture = (texture2D(uTexture, (vUv * 10.0)).rgb) * strength ;
+      vec3 texture = (texture2D(uTexture, (vUv * 50.0)).rgb) * strength ;
 
       gl_FragColor = vec4(texture, 1.0);
     }
@@ -62,21 +62,25 @@ const ExplosionMaterial = shaderMaterial(
 extend({ ExplosionMaterial });
 
 function BackgroundPlane() {
-
   const texture = useTexture(backgroundImage);
   texture.magFilter = THREE.NearestFilter;
   texture.minFilter = THREE.LinearMipMapLinearFilter;
   texture.wrapS = THREE.RepeatWrapping;
   texture.wrapT = THREE.RepeatWrapping;
-  texture.repeat.set(10, 10);
+  texture.repeat.set(50, 50);
 
   const ref = useRef();
 
-  let customUv = new THREE.Vector2(1, 1)
+  // let customUv = new THREE.Vector2(1, 1);
+
+  const [creationTimer, setCreationTimer] = useState(0);
+
+  // let creationTimer = 1;
 
   useFrame(({ clock }) => {
     ref.current.uTime = clock.getElapsedTime();
-    ref.current.uCustomUv = customUv
+    // ref.current.uCustomUv = customUv;
+    ref.current.uCreationTimer = creationTimer;
   });
 
   useEffect(() => {
@@ -85,13 +89,48 @@ function BackgroundPlane() {
     // ref.current.wireframe = true
   }, []);
 
-  
+  const [playCreation, setPlayCreation] = useState(false)
+
+  useEffect(() => {
+    if ( playCreation === true){
+      const intervalId = setInterval(() => {
+        if (creationTimer > 3) {
+          // creationTimer = 1;
+          setCreationTimer(0)
+        } else {
+          // creationTimer += 1;
+          setCreationTimer( ( creationTimer) => creationTimer + 1)
+        }
+        console.log("timer", creationTimer);
+      }, 1000);
+      return () => clearInterval(intervalId);
+    }
+  }, [playCreation, creationTimer]);
 
   return (
     <>
-      <mesh onPointerMove={(e) => customUv = e.uv }>
-        <planeBufferGeometry args={[5, 5, 10, 10]} />
-        <explosionMaterial ref={ref} uTexture={texture} />
+      <mesh
+        onPointerMove={(e) => {
+          // (customUv = e.uv)
+          ref.current.uCustomUv = e.uv;
+        }}
+        onPointerDown={ () => {
+          setPlayCreation(true)
+        }}
+        onPointerUp={ () => {
+          setPlayCreation(false)
+        }}
+        // onClick={() => {
+          
+        // }}
+      >
+        <planeBufferGeometry args={[5, 5, 50, 50]} />
+        <explosionMaterial
+          ref={ref}
+          uTexture={texture}
+          // uVisible={visible}
+          uCreationTimer={creationTimer}
+        />
       </mesh>
     </>
   );
@@ -103,7 +142,7 @@ function App() {
       <Suspense fallback={null}>
         <Canvas>
           <ambientLight />
-          <OrbitControls />
+          {/* <OrbitControls /> */}
           <BackgroundPlane />
           <Stats />
         </Canvas>
