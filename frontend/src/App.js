b1ff4b9df90bd9eb5793eeb21c41c689a215f694
zhyoleh <zhinticholeh@gmail.com>
2022-03-25 06:41:59 -0600

    simple mushroom pattern

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -43,16 +43,32 @@ const ExplosionMaterial = shaderMaterial(
 
     varying vec2 vUv;
 
+    float random(vec2 st)
+{
+    return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
+}
+
     void main(){
 
-      float strength = distance(vUv, uCustomUv) / uCreationTimer;
+      // float strength = distance(vUv, uCustomUv) / uCreationTimer;
 
+      // float strength = distance(vUv, uCustomUv) / uCreationTimer;
 
-      if( strength > 0.1){
+      // float strength = distance(vec2((vUv.x - 0.5) * 0.5 + 0.5, (vUv.y - 0.5) * 0.8 + 0.5), vec2(0.5, 0.5));
+
+      float ellipse = distance(vec2(vUv.x, (vUv.y - 0.5) * 2.0 + 0.5), vec2(0.5));
+      float stem = distance(vec2((vUv.x - 0.5) * 5.0 + 0.5, (vUv.y + 0.25 )), vec2(0.5));
+
+      float strength = ellipse * stem / uCreationTimer;
+
+      float randomPoint = random(vUv) * 0.1;  
+
+      if( strength > randomPoint){
         strength = 1.0;
-      } else if( strength < 0.1){
+      } else if( strength < randomPoint){
         strength = 0.0; //this is the circle
       }
+
       vec3 texture = (texture2D(uTexture, (vUv * 50.0)).rgb) * strength;
 
       gl_FragColor = vec4(texture, 1.0);
@@ -94,7 +110,8 @@ function BackgroundPlane() {
     if (playCreation === true) {
       const intervalId = setInterval(() => {
         if (creationTimer > 3) {
-          setCreationTimer(0);
+          setPlayCreation(false)
+          setPlayDecay(true)
         } else {
           setCreationTimer((creationTimer) => creationTimer + 1);
         }
@@ -120,15 +137,16 @@ function BackgroundPlane() {
         onPointerMove={(e) => {
           // (customUv = e.uv)
           ref.current.uCustomUv = e.uv;
+          setPlayCreation(true)
         }}
-        onPointerDown={() => {
-          setPlayCreation(true);
-          setPlayDecay(false);
-        }}
-        onPointerUp={() => {
-          setPlayCreation(false);
-          setPlayDecay(true);
-        }}
+        // onPointerDown={() => {
+        //   setPlayCreation(true);
+        //   setPlayDecay(false);
+        // }}
+        // onPointerUp={() => {
+        //   setPlayCreation(false);
+        //   setPlayDecay(true);
+        // }}
       >
         <planeBufferGeometry args={[5, 5, 50, 50]} />
         <explosionMaterial
